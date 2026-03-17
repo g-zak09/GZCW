8a757278a36b355388d083fa6d40885745c45829
Your Name <you@example.com>
2026-03-17 11:16:30 +0000

    Install tables

---
+++ install.php
@@ -0,0 +1,85 @@
+<?php
+
+// Create database
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$conn= new PDO("mysql:host=$servername",$username,$password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$sql="CREATE DATABASE IF NOT EXISTS GZCW";
+$conn->exec($sql);
+$sql="USE GZCW";
+$conn->exec($sql);
+echo("DB created successfully<br>");
+
+// Create users table
+$stmt=$conn->prepare("DROP TABLE IF EXISTS tblusers;
+CREATE TABLE tblusers
+(UserID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY  KEY,
+AccountType ENUM(Admin, User) NOT NULL
+Forename VARCHAR(30) NOT NULL,
+Surname VARCHAR(30) NOT NULL,
+Username VARCHAR(30) NOT NULL,
+Password VARCHAR(225) NOT NULL,
+DateOfBirth DATE NOT NULL,
+Balance INT(9) NOT NULL,
+);
+");
+$stmt->execute();
+echo("tblusers created<br>");
+
+// Create sports table
+$stmt=$conn->prepare("DROP TABLE IF EXISTS tblsports;
+CREATE TABLE tblsports
+(SportID INT(3) UNSIGNED AUTO_INCREMENT PRIMARY  KEY,
+SportName VARCHAR(30) NOT NULL,
+);
+");
+$stmt->execute();
+echo("tblsports created<br>");
+
+// Create teams table
+$stmt=$conn->prepare("DROP TABLE IF EXISTS tblteams;
+CREATE TABLE tblteams
+(TeamID
+SportID INT(3) UNSIGNED AUTO_INCREMENT PRIMARY  KEY,
+TeamName VARCHAR(50) NOT NULL,
+);
+");
+$stmt->execute();
+echo("tblteams created<br>");
+
+// Create sporting events table
+$stmt=$conn->prepare("DROP TABLE IF EXISTS tblevents;
+CREATE TABLE tblevents
+(EventID INT(5) UNSIGNED AUTO_INCREMENT PRIMARY  KEY,
+SportID INT(3) UNSIGNED AUTO_INCREMENT FOREIGN KEY,
+HomeTeamID INT(4) UNSIGNED AUTO_INCREMENT FOREIGN KEY,
+AwayTeamID INT(4) UNSIGNED AUTO_INCREMENT FOREIGN KEY,
+EventDate DATE NOT NULL,
+EventTime TIME NOT NULL,
+HomeWinOdds DEC(4,2) NOT NULL,
+AwayWinOdds DEC(4,2) NOT NULL,
+Result ENUM(Home, Away, Draw, Pending) NOT NULL,
+);
+");
+$stmt->execute();
+echo("tblevents created<br>");
+
+// Create bets table
+$stmt=$conn->prepare("DROP TABLE IF EXISTS tblbets;
+CREATE TABLE tblbets
+(BetID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY  KEY,
+SportID INT(3) UNSIGNED AUTO_INCREMENT FOREIGN KEY,
+EventID INT(5) UNSIGNED AUTO_INCREMENT FOREIGN KEY,
+BetAmount INT(6) NOT NULL,
+UserBetPrediction ENUM(Home, Away, Draw) NOT NULL,
+UserBetResult ENUM(Win, Loss)
+BetDateTime DATETIME(0) NOT NULL,
+BetOdds DEC(4,2) NOT NULL,
+);
+");
+$stmt->execute();
+echo("tblbets created<br>");
+
+?>
